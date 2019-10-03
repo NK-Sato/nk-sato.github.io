30676b8d7b391d3a75e5a2806b648627440352de
Nk-Sato <sato@netkin.fr>
2019-10-03 14:51:51 +0200

    load camera input file into canvas

--- nk-pwa-qrcode/assets/imageCapture.js
+++ nk-pwa-qrcode/assets/imageCapture.js
@@ -1,9 +1,30 @@
 window.addEventListener('DOMContentLoaded', function () {
+
     let imgCapture = document.getElementById("imageCapture");
+    let canvas = document.getElementById("canvas");
+
     imgCapture.onchange = function(){
         let file = imgCapture.files;
-        for( var i = 0; i < file.length; i++){
-            console.log(file[i]);
-        }
+        drawOnCanvas(file);
+    }
+
+    function drawOnCanvas(file) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            var dataURL = e.target.result,
+                ctx = canvas.getContext('2d'),
+                img = new Image();
+
+            img.onload = function() {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+            };
+
+            img.src = dataURL;
+        };
+
+        reader.readAsDataURL(file);
     }
 });
